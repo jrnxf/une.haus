4b61f840174bc01017b99a9097aeac29f1b8deb7
Colby Thomas <colby@jrnxf.co>
2025-05-27 11:40:40 +0100

    squash [skip-ci]

--- src/lib/session/hooks.ts
+++ src/lib/session/hooks.ts
@@ -4,13 +4,14 @@ import {
   useNavigate,
   useRouteContext,
 } from "@tanstack/react-router";
+import { serverOnly } from "@tanstack/react-start";
 import { useSession } from "@tanstack/react-start/server";
 
 import { env } from "~/lib/env";
 import { HAUS_SESSION_KEY, session } from "~/lib/session/index";
 import { type HausSession } from "~/lib/session/schema";
 
-export const useServerSession = () => {
+export const useServerSession = serverOnly(() => {
   return useSession<HausSession>({
     name: HAUS_SESSION_KEY,
     password: env.SESSION_SECRET,
@@ -21,7 +22,7 @@ export const useServerSession = () => {
     },
     maxAge: 60 * 60 * 24 * 30, // 30 days
   });
-};
+});
 
 export function useSessionUser() {
   const { session } = useRouteContext({ from: rootRouteId });
